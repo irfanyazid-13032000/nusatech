0eb49d88f231dd19be5191ef5516eb377c8096eb
irfanyazid-13032000 <irfanyazid18@gmail.com>
2022-11-30 12:59:27 +0700

    membuar repo

---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use App\Http\Controllers\LoginController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('dashboard');
+});
+
+
+Route::get('/login',[LoginController::class,'login']);
+
+
+Route::post('/login', [LoginController::class,'authenticate']);
+
+Route::get('/token', function (Request $request) {
+    $token = $request->session()->token();
+ 
+    $token = csrf_token();
+
+    var_dump($token);
+ 
+    // ...
+});
+
+
+
+
